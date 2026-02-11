4729148d6e1d7ea2a78aceb92a936249a42c62ee
Ertyup <DbErtyup@users.noreply.github.com>
2026-02-11 20:44:17 +0300

    mobile update
    67

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
 const panel = document.getElementById("main-panel");
 const glow = document.querySelector(".mouse-glow");
+const isMobile = window.matchMedia("(max-width: 768px)").matches;
 
 panel.addEventListener("mousemove", (e) => {
   const rect = panel.getBoundingClientRect();
   const x = e.clientX - rect.left;
   const y = e.clientY - rect.top;
 
-  // Koordinatları set ediyoruz
   panel.style.setProperty("--x", `${x}px`);
   panel.style.setProperty("--y", `${y}px`);
 });
 
-// Fare içeri girdiğinde bir anlık yumuşama ekleyip sonra kaldırmak için:
 panel.addEventListener("mouseenter", () => {
   glow.style.transition = "opacity 0.4s ease, transform 0.4s ease-out";
-  // Kısa bir süre sonra takibi anlık (real-time) yapması için transition'ı azaltıyoruz
+
   setTimeout(() => {
     glow.style.transition = "opacity 0.4s ease";
   }, 400);
 });
+
+if (!isMobile) {
+  panel.addEventListener("mousemove", (e) => {
+    const rect = panel.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    panel.style.setProperty("--x", `${x}px`);
+    panel.style.setProperty("--y", `${y}px`);
+  });
+} else {
+  document.querySelector(".mouse-glow").style.display = "none";
+}
